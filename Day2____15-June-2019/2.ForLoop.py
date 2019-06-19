0b0aff4c8a0a977c4910e6f1f74da5c370da3768
poonam5248 <kaurpoonam5248@gmail.com>
2019-06-19 12:57:24 +0530

    Day2 Programs Edited

---
+++ Day2____15-June-2019/2.ForLoop.py
@@ -0,0 +1,45 @@
+# For Loop
+
+# Ist Program
+print("Result of 1st Program is: ")
+for i in range(0,10):
+    print(i,end=" ")
+    print("Hello",end="\n")
+    
+# 2nd Program
+a=[5,2,4,8]
+print("\nResult of 2nd Program is: ")
+for i in a:
+    print(i,end="\n")
+
+
+# 3rd Program
+print("\nResult of 3rd Program is: ")
+for i in range(10):
+    print(i,end="\n")
+
+
+# 4th Program
+print("\nResult of 4th Program is: ")
+for i in range(10):
+    print(i,i,end="\n")
+
+# 5th Program
+print("\nResult of 5th Program is: ")
+for i in range(10):
+    a='5'+' Hello'
+    print(a)
+
+# 6th Program
+print("\nResult of 6th Program is: ")
+a='Poonam'
+for i in a:
+    print(i)
+
+# 7th Program
+print("\nResult of 7th Program is: ")
+for i in range(10):
+    a=str(i)+' Hello'
+    print(a)
+    
+    
