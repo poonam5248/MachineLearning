9069c4a34fc48faedff116ce77ba4c8720472e03
poonam5248 <kaurpoonam5248@gmail.com>
2019-06-24 12:02:35 +0530

    Day7 Programs Added

---
+++ Day7____22-June-2019/1.RandomLib_Password.py
@@ -0,0 +1,28 @@
+# 1.
+##
+##import random as r
+##
+##d=['A','B','C','D','0','1']
+##otp=''
+##for i in range(0,4):
+##    otp=otp+r.choice(d)
+##print(otp)
+
+# 2.
+
+##import random as r
+##
+##d=['A','B','C','D','0','1']
+##otp=''
+##for i in range(0,4):
+##    r.shuffle(d)
+##    otp=otp+d[0]
+##print(otp)
+
+# 3.  Datetime
+
+import datetime as d
+print(d.datetime.now())
+
+
+
